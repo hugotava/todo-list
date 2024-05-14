3d201b20a232ca8218b12da67cad934a6e27137d
hugotava <hugotbar@gmail.com>
2024-05-14 09:50:52 -0300

    update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,5 +1,4 @@
-// TodoItem.js
-// TodoItem.js
+// TodoItem.js - https://hashnode.com/@hugotav
 import React from 'react';
 
 const TodoItem = ({ todo, toggleTodo }) => {
@@ -20,16 +19,3 @@ const TodoItem = ({ todo, toggleTodo }) => {
 };
 
 export default TodoItem;
-
-
-// import React from 'react';: This line imports the React library, which is necessary for creating React components.
-
-// const TodoItem = ({ todo }) => {: This declares a functional component named TodoItem, which accepts a prop named todo. This prop represents an individual todo item.
-
-// <div className="todo-item">: This creates a <div> element with a CSS class of "todo-item", providing a container for the todo item.
-
-// <input type="checkbox" checked={todo.completed} />: This creates a checkbox input field. The checked attribute is set based on the completed property of the todo item. If the todo item is completed, the checkbox will be checked; otherwise, it will be unchecked.
-
-// <p>{todo.text}</p>: This displays the text content of the todo item inside a <p> element.
-
-// export default TodoItem;: This exports the TodoItem component so that it can be imported and used in other parts of the application.
